a5c39bcfea217eae27ae2293084d580c6d17ed76
RichardMilos233 <richardmilos233@gmail.com>
2026-08-14 15:20:47 +0800

    add: demo formatted

--- demo.py
+++ demo.py
@@ -3,18 +3,18 @@ import cmath, math
 # u_tt - c^2 * u_xx = f
 
 eq = WaveEquation(
-    dim=1,                          # 1 space dimension
-    c=1j,                          # wave speed (c=1 -> ordinary, well-posed)
-    f={1:-1, 2:1},                           # no source term: u_tt = c² u_xx
+    dim=1,                                        # 1 space dimension
+    c=1j,                                         # wave speed (c=1 -> well-posed, c=1j -> ill-posed)
+    f={1:-1, 3:1},                                # outer force
     phi=lambda z: cmath.sin(math.pi * z),         # u(x, 0)
-    psi=lambda z: -cmath.sin(math.pi * z),               # initial velocity, zero
+    psi=lambda z: -cmath.sin(math.pi * z),        # u_t(x, 0)
     domain=((0.0, 1.0),),
     name="my_wave"
 )
 
 
-times = [0.0, 0.1, 0.2]
+times = [0.0, 0.1, 0.2, 0.5]
 fd = ExplicitFD(N=101, dt=0.002).solve(eq, times)
 rfd = RegularizedFD(N=101, dt=0.002, k_max=12).solve(eq, times)
-# mc = BranchingMC(lam=0.25, n=20_000, seed=0).solve(eq, times)
-compare(fd, rfd).plot("demo.png")
+mc = BranchingMC(lam=0.25, n=20_000, seed=0).solve(eq, times)
+compare(fd, rfd, mc).plot("demo.png")
